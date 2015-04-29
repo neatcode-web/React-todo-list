b8a085e81673f83c4031ef1c5ea6dc3e0a17e6b5
neatcode-web <williamfeng1001@gmail.com>
2015-04-29 21:25:00 +0530

    Added comments to code

--- app/js/application.js
+++ app/js/application.js
@@ -6,17 +6,23 @@ JS : MY CUSTOM SCRIPTS
 
 var TodoList = React.createClass({
   render: function() {
+    
     var createItem = function(itemText, index) {
-      return <p key={index + itemText}>{itemText}</p>;
+      return <li key={index + itemText}>{itemText}</li>;
     };
-    return <div className="list-container">{this.props.items.map(createItem)}</div>;
+    
+    return <ul className="list-container">{this.props.items.map(createItem)}</ul>;
   }
 });
 
 var ToDoApp = React.createClass({
+	
+	//Create an array and empty text on intialization
 	getInitialState: function() {
 		return {items: [], text: ''};
 	},
+	
+	//Added a new list
 	addToList: function(e) {
 		if(this.state.text === '') {
 			return;
@@ -27,30 +33,32 @@ var ToDoApp = React.createClass({
 
     this.setState({items: nextItems, text: nextText});
 		
+		//Create new element to append to list
 		var createNewEle = function(itemText, index) {
 			return <p key={index + itemText}>{itemText}</p>
 		}
 
-		console.log(this.props.items.map(createNewEle))
-		// return this.props.items.map(createNewEle);
+		return this.props.items.map(createNewEle);
 	},
+	//On change of test update the text 
 	onChange: function(e) {
 		this.setState({text: e.target.value});
 	},
+	//To Render the todo list app
 	render: function() {
 	  return (
-
+	  	//HTML content to be rendered
 	  	<div className="main-container">
-	  		<h2>items List</h2>
+	  		<h2>Todo List</h2>
 	  		<input className="add-to-list" placeholder="Add to list" onChange={this.onChange} value={this.state.text}/>
 	  		<button className="add-btn" onClick={this.addToList}>Add</button>
 
-				<TodoList items={this.state.items}/>
+			<TodoList items={this.state.items}/>
 	  	</div>  	
 
 	  );
 	}
 });
 
-//Instruct the page to use above component
+//Instruct the page to use above created component
 React.render(<ToDoApp />, document.body);
